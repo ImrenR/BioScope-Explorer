03eeec4134796e80a7b3055ada00955427dc43ae
ImrenR <imrenrahbay@gmail.com>
2025-09-07 22:50:43 +0100

    search button

--- src/components/BacteriaList.jsx
+++ src/components/BacteriaList.jsx
@@ -1,9 +1,24 @@
 const BacteriaList = () => {
-  return(
-     <div>
-
+  return (
+    <div className="container mt-5 d-flex justify-content-center">
+  <div className="input-group w-100">
+    <input
+      type="text"
+      className="form-control"
+      list="datalistOptions"
+      placeholder="Type to search..."
+    />
+    <button className="btn btn-outline-primary" type="submit">
+      Search
+    </button>
+    <datalist id="datalistOptions">
+      <option value="..." />
+    
+    </datalist>
   </div>
-  )
-}
+</div>
 
-export default BacteriaList
+  );
+};
+
+export default BacteriaList;
