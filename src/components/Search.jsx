51f3eb1498438611253cd391ce03495280897fb0
ImrenR <imrenrahbay@gmail.com>
2025-09-08 16:08:22 +0100

    Search styled

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,36 +1,28 @@
 const Search = () => {
   return (
-    <div>
-      <div>
-        <div className="container d-flex justify-content-center mt-5">
-          <div className="input-group">
-            <input
-              type="text"
-              className="form-control"
-              list="datalistOptions"
-              placeholder="Type to search..."
-            />
-            <button className="btn btn-outline-primary" type="submit">
-              Search
-            </button>
-            <datalist id="datalistOptions">
-              <option value="..." />
-            </datalist>
-          </div>
-        </div>
-        <div className="m-4 text-center">
-          <h4>Welcome to the Bioscope Web</h4>
-          <h6>
-            <i>
-              10,000+ inspirational bacteria designs, illustrations and graphic
-              elements
-            </i>
-          </h6>
-        </div>
+    <>
+    <form action="">
+      <div className=" container-sm  d-flex justify-content-center mt-5 gap-1">
+        <input
+          type="text"
+          className="form-control w-25"
+          placeholder="Type to search..."
+        />
+        <button className="btn btn-outline-primary" type="submit">
+          Search
+        </button>
       </div>
-
-      
-    </div>
+</form>
+      <div className="mt-4 text-center">
+        <h4 className="text text-fw-bolder">Welcome to the Bioscope Web</h4>
+        <h6 className="fw-lighter">
+         
+            10,000+ inspirational bacteria designs, illustrations and graphic
+            elements
+         
+        </h6>
+      </div>
+    </>
   );
 };
 
