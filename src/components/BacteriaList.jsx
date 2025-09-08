7d867b5bbfbbfcdc32baad13b21cedd3a3468daa
ImrenR <imrenrahbay@gmail.com>
2025-09-08 16:45:32 +0100

    update

--- src/components/BacteriaList.jsx
+++ src/components/BacteriaList.jsx
@@ -7,8 +7,8 @@ const BacteriaList = ({ bacteria }) => {
           const imageUrl = thumbnail?.url || "https://via.placeholder.com/150"; // fallback image
 
           return (
-            <div key={index} className="col-12 col-sm-6 col-md-3 mb-4 mt-4">
-             <div className="card h-100">
+            <div key={index} className="col-12 col-sm-6 col-md-3 mb-4 mt-4 ">
+             <div className="card overflow-hidden">
                 <img src={imageUrl} className="card-img-top" />
                 <div className="card-body">
                   <h5 className="card-title">{key}</h5>
