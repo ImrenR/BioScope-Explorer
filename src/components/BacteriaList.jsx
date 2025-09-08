61e0d37cd5d1a222c5dac8ea21cf658dcd0af5a6
ImrenR <imrenrahbay@gmail.com>
2025-09-08 01:38:08 +0100

    update

--- src/components/BacteriaList.jsx
+++ src/components/BacteriaList.jsx
@@ -1,16 +1,25 @@
-const BacteriaList = ({ bacteria, getData }) => {
+const BacteriaList = ({ bacteria }) => {
   return (
-    <div className="container row ">
-      {bacteria?.map((item, index) => {
-        const { thumbnail, key, description } = item;
-        return (
-          <div key={index} className= "col-12 col-sm-6 col-md-3 mb-4 mt-4">
-            {thumbnail?.url && <img src={thumbnail} alt={key} />}
-            <p>{key}</p>
-            <p>{description}</p>
-          </div>
-        );
-      })}
+    <div className="container ">
+      <div className="row ">
+        {bacteria?.map((item, index) => {
+          const { thumbnail, key, description } = item;
+          const imageUrl = thumbnail?.url || "https://via.placeholder.com/150"; // fallback image
+
+          return (
+            <div key={index} className="col-12 col-sm-6 col-md-3 mb-4 mt-4">
+             <div className="card h-100">
+                <img src={imageUrl} className="card-img-top" />
+                <div className="card-body">
+                  <h5 className="card-title">{key}</h5>
+                  <p className="card-text">{description}</p>
+                </div>
+              </div>
+            
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
