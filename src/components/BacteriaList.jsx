e08d7718da2f6d68e83d246d7a0537c283ea16c6
ImrenR <imrenrahbay@gmail.com>
2025-09-08 01:23:22 +0100

    data rendered

--- src/components/BacteriaList.jsx
+++ src/components/BacteriaList.jsx
@@ -1,12 +1,16 @@
-import Filter from "./Search";
-import LinkList from "./LinkList";
-
-const BacteriaList = () => {
+const BacteriaList = ({ bacteria, getData }) => {
   return (
-    <div>
-      <div>
-        <img src="" alt="" />
-      </div>
+    <div className="container row ">
+      {bacteria?.map((item, index) => {
+        const { thumbnail, key, description } = item;
+        return (
+          <div key={index} className= "col-12 col-sm-6 col-md-3 mb-4 mt-4">
+            {thumbnail?.url && <img src={thumbnail} alt={key} />}
+            <p>{key}</p>
+            <p>{description}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,33 @@
+import axios from "axios";
 import BacteriaList from "../components/BacteriaList";
 import LinkList from "../components/LinkList";
 import NavBar from "../components/NavBar";
 import Search from "../components/Search";
-
+import { useEffect, useState } from "react";
 const Home = () => {
+  const [bacteria, setBacteria] = useState([]);
+
+  const getData = async () => {
+    try {
+      const res = await axios.get(import.meta.env.VITE_APP_URL);
+      setBacteria(res.data.pages);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+useEffect(() => {
+   getData();
+}, [])
+
+
+ 
   return (
     <>
       <NavBar />
-      <Search/>
+      <Search />
       <LinkList />
-      <BacteriaList />
+      <BacteriaList bacteria={bacteria} getData={getData}/>
     </>
   );
 };
