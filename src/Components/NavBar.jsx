28fff0debe5c3657e14f7286c2b11d0018818b88
ImrenR <imrenrahbay@gmail.com>
2025-09-12 21:50:24 +0100

    update

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -19,32 +19,7 @@ const NavBar = ({resetAll}) => {
         >
           <span className="navbar-toggler-icon" />
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                About
-              </a>
-            </li>
-           
-          </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-primary" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
+       
       </div>
     </nav>
   );
