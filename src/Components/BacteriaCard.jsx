01db054beb59cfbc74d5b53d02a7fba0c0328d17
ImrenR <imrenrahbay@gmail.com>
2025-09-12 16:25:25 +0100

    update

--- src/Components/BacteriaCard.jsx
+++ src/Components/BacteriaCard.jsx
@@ -1,23 +1,25 @@
 const BacteriaCard = ({ BacteriaData }) => {
   return (
     <div className="container mt-4">
-      <div className="row">
+      <div className="row g-4">
         {BacteriaData.map((item, id) => {
           const { name, group, image, description } = item;
           return (
-            <div key={id} className="col-12 col-sm-6 col-md-4 col-l-3 mb-4">
-              <div className="card d-flex flex-column align-items-center" style={{ width: "18rem" }}>
-                <img src={image} className="card-img-top w-75" alt={name} />
-                <div className="card-body ">
+            <div key={id} className="col-12 col-md-6 col-lg-4">
+              <div className="card" >
+                <img src={image} className="card-img-top" alt={name} />
+                <div className="card-body d-block">
                   <h5 className="card-title">{name}</h5>
-                  <p className="card-text">{description}</p>
+                  <p className="card-text lh-sm">{description}</p>
+                  <div className="group d-flex flex-column align-items-center">
                   <h6 className="card-group">{group}</h6>
                   <a
                     href="#"
-                    className="btn btn-primary mt-auto"
+                    className="btn btn-primary"
                   >
                     Click to see more..
                   </a>
+                  </div>
                 </div>
               </div>
             </div>
