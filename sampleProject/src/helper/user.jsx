9bb60580a2b07fd3ce30f88166097b4901f71c07
ImrenR <imrenrahbay@gmail.com>
2025-09-12 13:43:10 +0100

    filtering

---
+++ sampleProject/src/helper/user.jsx
@@ -0,0 +1,74 @@
+const Users = [
+    {
+        "id": 1,
+        "first_name": "Emiline",
+        "last_name": "McClune",
+        "email": "emcclune0@xrea.com",
+        "gender": "Female"
+    },
+    {
+        "id": 2,
+        "first_name": "Felix",
+        "last_name": "Ingleston",
+        "email": "fingleston1@hibu.com",
+        "gender": "Female"
+    },
+    {
+        "id": 3,
+        "first_name": "Travus",
+        "last_name": "Bergstram",
+        "email": "tbergstram2@pbs.org",
+        "gender": "Female"
+    },
+    {
+        "id": 4,
+        "first_name": "Lydia",
+        "last_name": "Harrington",
+        "email": "lharrington3@example.com",
+        "gender": "Female"
+    },
+    {
+        "id": 5,
+        "first_name": "Marcus",
+        "last_name": "Patterson",
+        "email": "mpatterson4@example.com",
+        "gender": "Male"
+    },
+    {
+        "id": 6,
+        "first_name": "Sophia",
+        "last_name": "Williams",
+        "email": "swilliams5@example.com",
+        "gender": "Female"
+    },
+    {
+        "id": 7,
+        "first_name": "Ethan",
+        "last_name": "Clark",
+        "email": "eclark6@example.com",
+        "gender": "Male"
+    },
+    {
+        "id": 8,
+        "first_name": "Isabella",
+        "last_name": "Johnson",
+        "email": "ijohnson7@example.com",
+        "gender": "Female"
+    },
+    {
+        "id": 9,
+        "first_name": "Oliver",
+        "last_name": "Smith",
+        "email": "osmith8@example.com",
+        "gender": "Male"
+    },
+    {
+        "id": 10,
+        "first_name": "Ava",
+        "last_name": "Brown",
+        "email": "abrown9@example.com",
+        "gender": "Female"
+    }
+]
+
+export default Users;
