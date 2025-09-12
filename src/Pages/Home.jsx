07d2cd3382db645e45b9f6f4468afeeeea5ae313
ImrenR <imrenrahbay@gmail.com>
2025-09-12 19:35:38 +0100

    filter and dropdown

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,38 +1,38 @@
-import { useState } from "react"
-import BacteriaCard from "../Components/BacteriaCard"
-import BacteriaData from "../Helper/Bacterium"
-import NavBar from "../Components/NavBar"
-
-
+import { useState } from "react";
+import BacteriaCard from "../Components/BacteriaCard";
+import BacteriaData from "../Helper/Bacterium";
+import NavBar from "../Components/NavBar";
+import GramPN from "../Components/GramPN";
 
 const Home = () => {
+  const [search, setSearch] = useState("");
 
-  const [search, setSearch] = useState("")
-
-const filtered = BacteriaData.filter(item=>
-  item.name.toLowerCase().includes(search.toLowerCase()) ||
-  item.group.toLowerCase().includes(search.toLowerCase())
-)
+  const filtered = BacteriaData.filter(
+    (item) =>
+      item.name.toLowerCase().includes(search.toLowerCase()) ||
+      item.group.toLowerCase().includes(search.toLowerCase())
+  );
   return (
-  <div className="app">
-    <NavBar/>
-   <div className="container-fuild">
-    <div className="row justify-container-center">
-      <div className="col-12 col-lg-10 col-xl-8">
+    <div className="app">
+      <div className="container-fuild">
+        <div className="row justify-container-center">
+          <NavBar />
+          <div className="col-12 col-lg-10 col-xl-8">
             <div className="mb-4">
-      <input 
-      type="text" 
-      className="Search" 
-      placeholder="Search.."
-      onChange={(e)=>setSearch(e.target.value)}/>
-     
-       </div>
- </div>
- </div>
-<BacteriaCard BacteriaData={filtered}/>
-</div>
- </div>
-)
-}
+              <input
+                type="text"
+                className="Search"
+                placeholder="Search.."
+                onChange={(e) => setSearch(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+        <GramPN/>
+        <BacteriaCard BacteriaData={filtered} />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
