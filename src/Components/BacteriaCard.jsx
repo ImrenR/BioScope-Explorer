80bd0b7156449307b5c161fad8f6c4ea3df3aa25
ImrenR <imrenrahbay@gmail.com>
2025-09-12 15:35:02 +0100

    styling the cards

--- src/Components/BacteriaCard.jsx
+++ src/Components/BacteriaCard.jsx
@@ -1,20 +1,20 @@
 const BacteriaCard = ({ BacteriaData }) => {
   return (
     <div className="container mt-4">
-      <div className="row justify-content-center align-items-center">
+      <div className="row">
         {BacteriaData.map((item, id) => {
           const { name, group, image, description } = item;
           return (
-            <div key={id} className="col col-12 col-lg-10 col-xl-8">
-              <div className="card" style={{ width: "18rem" }}>
-                <img src={image} className="card-img-top" alt="images" />
-                <div className="card-body">
+            <div key={id} className="col-12 col-sm-6 col-md-4 col-l-3 mb-4">
+              <div className="card d-flex flex-column align-items-center" style={{ width: "18rem" }}>
+                <img src={image} className="card-img-top w-75" alt={name} />
+                <div className="card-body ">
                   <h5 className="card-title">{name}</h5>
                   <p className="card-text">{description}</p>
                   <h6 className="card-group">{group}</h6>
                   <a
                     href="#"
-                    className="btn btn-primary bg-body-secondary border-0 text-black"
+                    className="btn btn-primary mt-auto"
                   >
                     Click to see more..
                   </a>
